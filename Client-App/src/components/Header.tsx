c7389a3b050cddc6fdbf44265e9fa787552896f3
Dimitar Kuzmanov <mitkokuzmanov@gmail.com>
2025-03-01 14:32:11 +0200

    App.css changed and header created.
    App.css with preset values deleted. Header was created.

---
+++ Client-App/src/components/Header.tsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <>
+      <div className="header">
+        <h1 className="header__logo">PRO-Folio</h1>
+       
+          <ul className="header__navbox">
+            <li>Portfolios</li>
+            <li>About Us</li>
+            <li>Contacts</li>
+          </ul>
+        
+      </div>
+    </>
+  );
+}
